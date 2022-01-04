14a075bae6e9c3cb640f2ac8efb039de930a7032
Vlad Danilov <vladislavs.danilovs@inbox.lv>
2022-01-04 10:03:43 +0200

    add synchronized collection

---
+++ Lesson13_Multithreading/Example31_SynchronizedCollection.java
@@ -0,0 +1,26 @@
+package Lesson13_Multithreading;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Example31_SynchronizedCollection {
+    public static void main(String[] args) throws InterruptedException {
+        ArrayList<Integer> source =new ArrayList<>();
+        for(int i=0; i<5; i++){
+            source.add(i);
+        }
+//        ArrayList<Integer> target =new ArrayList<>();
+        List<Integer> synchList =
+                Collections.synchronizedList(new ArrayList<>());
+        Runnable runnable = () -> {synchList.addAll(source);};
+
+        Thread thread1 = new Thread(runnable);
+        Thread thread2 = new Thread(runnable);
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+        System.out.println(synchList);
+    }
+}
