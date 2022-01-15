921acd0479340a322ef444f99a38a9b27083e8ab
Vlad Danilov <vladislavs.danilovs@inbox.lv>
2022-01-15 11:22:10 +0200

    add Annotation

---
+++ Lesson16_Another_Important_Topics/Annotation/Example1_Annotation1.java
@@ -0,0 +1,31 @@
+package Lesson16_Another_Important_Topics.Annotation;
+
+public class Example1_Annotation1 {
+    public static void main(String[] args) {
+        Parent p = new Child("David");
+        p.showInfo();
+    }
+}
+
+class Parent {
+    String name;
+
+    public Parent(String name) {
+        this.name = name;
+    }
+    @Deprecated
+    void showInfo() {
+        System.out.println("It's Parent class. Name = " + name);
+    }
+}
+
+class Child extends Parent {
+    public Child(String name) {
+        super(name);
+    }
+
+//    @Override
+    void showInfo() {
+        System.out.println("It's Child class. Name = " + name);
+    }
+}
---
+++ Lesson16_Another_Important_Topics/Annotation/Example2_Annotation2.java
@@ -0,0 +1,37 @@
+package Lesson16_Another_Important_Topics.Annotation;
+
+import java.lang.annotation.*;
+
+public class Example2_Annotation2 {
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class xiaomiClass = Class.forName("Lesson16_Another_Important_Topics.Annotation.Xiaomi");
+       Annotation annotation1 = xiaomiClass.getAnnotation(SmartPhone.class);
+       SmartPhone sm1 = (SmartPhone) annotation1;
+        System.out.println("Annotation info from Xiaomi class: " +
+                sm1.OS() + ", "+ sm1.yearOfCompanyCreation());
+
+        Class iphoneClass = Class.forName("Lesson16_Another_Important_Topics.Annotation.Iphone");
+        Annotation annotation2 = iphoneClass.getAnnotation(SmartPhone.class);
+        SmartPhone sm2 = (SmartPhone) annotation2;
+        System.out.println("Annotation info from Xiaomi class: " +
+                sm2.OS() + ", "+ sm2.yearOfCompanyCreation());
+    }
+}
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@interface SmartPhone{
+    String OS() default "Android"; //Syntax of writing variables in @
+    int yearOfCompanyCreation() default 2010;
+}
+
+@SmartPhone()
+class Xiaomi{
+    String model;
+    double price;
+}
+
+@SmartPhone(OS = "IOS", yearOfCompanyCreation = 1976)
+class Iphone{
+    String model;
+    double price;
+}
